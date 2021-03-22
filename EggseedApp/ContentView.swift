ad8b1b4364ff9c060cb22771aa9580ef32b0f120
Leo Dion <leogdion@brightdigit.com>
2021-03-22 19:40:50 -0400

    adding template list

--- EggseedApp/ContentView.swift
+++ EggseedApp/ContentView.swift
@@ -11,11 +11,25 @@ struct ContentView: View {
     var body: some View {
       List{
         Section(header: Text("Application")){
-          LazyHGrid(rows: [GridItem(.flexible(minimum: 86, maximum: 86))], content: {
-            TemplateView()
-            TemplateView()
-            TemplateView()
-            TemplateView()
+          LazyHGrid(rows: [GridItem(.fixed(100)),GridItem(.fixed(100)),GridItem(.fixed(100)),GridItem(.fixed(100))], content: {
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+          })
+        }
+        Section(header: Text("Libraries")){
+          LazyHGrid(rows: [GridItem(.fixed(100)),GridItem(.fixed(100)),GridItem(.fixed(100)),GridItem(.fixed(100))], content: {
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
+            TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application"))
           })
         }
       }
--- EggseedApp/TemplateView.swift
+++ EggseedApp/TemplateView.swift
@@ -7,7 +7,32 @@
 
 import SwiftUI
 
+struct Template {
+  let imageName : String
+  let label : String
+}
+
+/*
+001-blood-drop
+002-command-line
+003-command-line-1
+004-lambda
+005-logistics
+006-argument
+007-two-books
+008-framework
+009-gear
+010-tool-box
+011-box
+012-archive-black-box
+013-box-1
+014-content
+015-web-development
+016-quill
+*/
+
 struct TemplateView: View {
+  let template : Template
     var body: some View {
       VStack{
         ZStack{
@@ -19,7 +44,8 @@ struct TemplateView: View {
             let offsetX = (geometry.size.width - length) / 2.0
             let offsetY = (geometry.size.height - length) / 2.0
             VStack{
-              Image(systemName: "cloud.fill").resizable()
+              Image(template.imageName
+            ).resizable()
                 .aspectRatio(contentMode: .fit)
                 .padding(8.0).frame(width: length, height: length, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).offset(x: offsetX, y: offsetY)
               
@@ -29,14 +55,14 @@ struct TemplateView: View {
         Text("Vapor Server-Side Application")
           .font(.subheadline)
           .multilineTextAlignment(.center)
-          .padding(.top, 4.0)
+          .padding(.vertical, 4.0)
           .layoutPriority(1)
-      }
+      }.aspectRatio(/*@START_MENU_TOKEN@*/1.5/*@END_MENU_TOKEN@*/, contentMode: .fit).padding(8.0)
     }
 }
 
 struct TemplateView_Previews: PreviewProvider {
     static var previews: some View {
-      TemplateView().previewLayout(.fixed(width: 120, height: 100))
+      TemplateView(template: Template(imageName: "001-blood-drop", label: "Vapor Server-Side Application")).previewLayout(.fixed(width: 120, height: 100))
     }
 }
